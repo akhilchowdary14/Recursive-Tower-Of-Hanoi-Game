de004a3027e951bd6f26b4cc901720c994b0e748
akhilchowdary14 <chowdaryakhil743@gmail.com>
2026-03-11 15:06:21 +0530

    Refactor generateMoves function and add autoSolve

--- dsa.js
+++ dsa.js
@@ -1,4 +1,4 @@
-function generateMoves(n, from, to, aux, moves){
+function generateMoves(n,from,to,aux,moves){
 
 if(n===1){
 moves.push([from,to])
@@ -6,7 +6,31 @@ return
 }
 
 generateMoves(n-1,from,aux,to,moves)
+
 moves.push([from,to])
+
 generateMoves(n-1,aux,to,from,moves)
+
+}
+
+function autoSolve(){
+
+const moves=[]
+
+generateMoves(diskCount,"A","C","B",moves)
+
+let i=0
+
+const interval=setInterval(()=>{
+
+if(i>=moves.length){
+clearInterval(interval)
+return
+}
+
+moveDisk(moves[i][0],moves[i][1])
+i++
+
+},500)
 
 }
